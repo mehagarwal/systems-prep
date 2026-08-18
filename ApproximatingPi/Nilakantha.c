ceaecd903e34652f69c0dc4fff5aab37bd9b0f72
Meha Agarwal <mehaagarwal@mehas-mbp.lan>
2026-08-17 18:42:20 -0700

    Setup

---
+++ ApproximatingPi/Nilakantha.c
@@ -0,0 +1,15 @@
+// Program to approximate value of pi using the Nilakantha series.
+// Takes an argument to see how many terms of the series need to be included in the calculation
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Main function that will accept arguments and return failure if argument not provided
+// Also parse to extract number of terms.
+
+// Function that computes the series
+
+//Function that returns the nth term
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+A series of small programs to refamiliarise myself with C/C++ and practise basic systems thinking.
+No AI has been used in the writing of any of this code.
